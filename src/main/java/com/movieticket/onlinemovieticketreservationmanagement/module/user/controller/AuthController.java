b1c35c9aef1613921d85690d089fcd376928c258
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-05-17 18:24:02 +0530

    AuthService Change

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/controller/AuthController.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/controller/AuthController.java
@@ -11,19 +11,35 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/auth")
-@RequiredArgsConstructor
+/*
+* Controller class for handling authentication operation
+* such as user login and registration
+*/
+
+@RestController//marks this class as a rest API controller
+@RequestMapping("/api/auth")//Base URL for authentication endpoints
+@RequiredArgsConstructor//automatically creates constructor for final fields
 public class AuthController {
 
+    //service layer object for authentication logic
     private final AuthService authService;
 
+
+    //login endpoint
+    //URL:POST/api/auth/login
+    //@param request contain user email and password
+    //@return JWT token or authentication response
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(
             @Valid @RequestBody LoginRequest request) {
 
         return ResponseEntity.ok(authService.login(request));
     }
+    //Register endpoint
+    //URL:POST/api/auth/Register
+    //@param request contain user register details
+    //@return authentication response after successful registration
+
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(

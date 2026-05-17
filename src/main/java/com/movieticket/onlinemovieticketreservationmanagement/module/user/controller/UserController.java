56dac9675a0bec0deb7491d34b67dbd052c64932
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-05-17 18:38:12 +0530

    AuthService Change

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/controller/UserController.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/controller/UserController.java
@@ -9,18 +9,27 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+//control class for managing user-related operations
+//registration and login
 
-@RestController
-@RequestMapping("/api/users")
-@RequiredArgsConstructor
+@RestController//marks this class as a REST controller
+@RequestMapping("/api/users")//Base URL for user endpoint
+@RequiredArgsConstructor//Generate constructor for final fields automatically
 public class UserController {
 
+    //service layer object for user operations
     private final UserService userService;
 
+    //User register endpoint
+    //request contain user register details
+    //registers registered user information
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest request) {//new user register to call service
         return ResponseEntity.status(201).body(userService.register(request));
     }
+    //user login endpoint
+    //@param request Contains email, passward
+    //@return authentication response with token/details
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {

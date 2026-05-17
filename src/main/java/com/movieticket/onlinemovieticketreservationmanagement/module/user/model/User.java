f420e0200ed78e27697d68c79b7e40023eff7ae6
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-05-17 15:45:18 +0530

    user have name change as full name

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/model/User.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/model/User.java
@@ -16,7 +16,7 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
-    private String name;
+    private String fullName;
 
     @Column(nullable = false, unique = true)
     private String email;

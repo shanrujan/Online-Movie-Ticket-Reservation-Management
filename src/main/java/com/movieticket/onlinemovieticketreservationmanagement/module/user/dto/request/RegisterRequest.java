804cf6cdaf7c817e369bd0fd8ca88323fed631b3
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-05-17 19:08:43 +0530

    AuthService Change

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/dto/request/RegisterRequest.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/dto/request/RegisterRequest.java
@@ -19,8 +19,7 @@ public class RegisterRequest {
     @NotBlank(message = "Password is required")
     private String password;
 
-    @NotNull(message = "Role is required")
-    private Role role;
+
 
     // Admin management key
     private String adminKey;

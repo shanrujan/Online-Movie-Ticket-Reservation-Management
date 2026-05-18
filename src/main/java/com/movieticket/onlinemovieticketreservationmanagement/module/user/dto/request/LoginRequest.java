13f8488a590be7952ad9b257bdfa8801c466246c
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-05-18 12:50:39 +0530

    add password size

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/dto/request/LoginRequest.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/dto/request/LoginRequest.java
@@ -2,6 +2,7 @@ package com.movieticket.onlinemovieticketreservationmanagement.module.user.dto.r
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,5 +13,6 @@ public class LoginRequest {
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min=6,message="password must be at least 6 characters")
     private String password;
 }

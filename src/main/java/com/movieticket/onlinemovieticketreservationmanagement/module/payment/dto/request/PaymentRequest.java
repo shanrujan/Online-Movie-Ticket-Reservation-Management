a276c923aec569d7bf5210a065205bcc0292bc8a
IT25102576 <it25102576@my.sliit.lk>
2026-05-18 17:16:45 +0530

    Added PaymentRequest DTO with booking ID and payment method validation

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/payment/dto/request/PaymentRequest.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/payment/dto/request/PaymentRequest.java
@@ -6,10 +6,10 @@ import lombok.Data;
 
 @Data
 public class PaymentRequest {
-
+    // Stores booking ID related to the payment
     @NotNull(message = "Booking ID is required")
     private Long bookingId;
-
+    // Stores payment method such as CARD, CASH, or UPI
     @NotBlank(message = "Payment method is required")
     private String paymentMethod; // CARD, CASH, UPI
 }

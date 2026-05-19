56d56fd0bf564e63667e5c1d23d8bb009be90fca
ImaWijesekara <218763457+ImaWijesekara@users.noreply.github.com>
2026-05-19 17:26:44 +0530

    user find by use name kind fix

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/repository/UserRepository.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/user/repository/UserRepository.java
@@ -7,5 +7,11 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    // 3. Helper checks for Registration (to make sure no duplicates exist)
+    boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
 }

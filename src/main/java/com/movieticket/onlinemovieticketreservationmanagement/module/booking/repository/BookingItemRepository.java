eea75a33be6e08c110bebd9cf4300a390721bb0d
avindu-abhisheka <avinduabhisheka@gmail.com>
2026-05-19 13:20:27 +0530

    Update BookingItemRepository.java

--- src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/booking/repository/BookingItemRepository.java
+++ src/main/java/com/movieticket/onlinemovieticketreservationmanagement/module/booking/repository/BookingItemRepository.java
@@ -11,11 +11,11 @@ public interface BookingItemRepository extends JpaRepository<BookingItem, Long> 
 
     List<BookingItem> findByBookingId(Long bookingId);
 
-    @Query("""
-            SELECT CASE WHEN COUNT(bi) > 0 THEN true ELSE false END
-            FROM BookingItem bi
-            WHERE bi.seat.id = :seatId
-            AND bi.booking.showtime.id = :showtimeId
+    @Query("""howtimeId
+                                                                SELECT CASE WHEN COUNT(bi) > 0 THEN true ELSE false END
+                                                                FROM BookingItem bi
+                                                                WHERE bi.seat.id = :seatId
+                                                                AND bi.booking.showtime.id = :s
             AND bi.booking.status = 'CONFIRMED'
             """)
     boolean existsBookedSeat(
